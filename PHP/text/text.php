914e35775ed18b774ae2b915ed19bc7be44f747e
Fr33Gn3M <freedomd3@gmail.com>
2018-07-13 15:00:50 +0800

    2

--- PHP/text/text.php
+++ PHP/text/text.php
@@ -6,7 +6,17 @@ echo $b; // outputs Hello world!
 echo "<br />";
 
 $y = 234;
-echo $y;
+echo $y."<br />";
 
-echo $y;
+$arr = ["text1","car1","name1"];
+for($x=0;$x<count($arr);$x++){
+    echo $arr[$x];
+    echo "<br />";
+}
+$arr2 = array('a1' => 34, 'a2'=>35);
+foreach ($arr2 as $key => $value) {
+    # code...
+    echo $key . $value;
+    echo "<br />";
+}
 ?>
