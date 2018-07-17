89c83389949aabacf56f990b9b46e9cf96600935
Fr33Gn3M <freedomd3@gmail.com>
2018-07-17 16:46:41 +0800

    new

--- PHP/study/array.php
+++ PHP/study/array.php
@@ -1,45 +1,41 @@
 <?php
-echo "数组1正序：<br />";
-$arr = ["text1","car1","name1"];
-sort($arr);
-for($x=0;$x<count($arr);$x++){
-    echo $arr[$x];
-    echo "<br />";
-}
+    #数值数组,两种定义方式
+    $arr = array('a','b','c');
+    $brr[0] = 'a';
+    $brr[1] = 'b';
+    $brr[2] = 'c';
 
-echo "数组1根据键正序排序：<br />";
-ksort($arr);
-for($x=0;$x<count($arr);$x++){
-    echo $arr[$x];
-    echo "<br />";
-}
-
-echo "数组1根据值正序排序：<br />";
-asort($arr);
-for($x=0;$x<count($arr);$x++){
-    echo $arr[$x];
-    echo "<br />";
-}
-echo "数组2逆序排序：";
-$arr2 = array('a1' => 34, 'a2'=>35);
-rsort($arr2);
-foreach ($arr2 as $key => $value) {
-    # code...
-    echo $key . $value;
-    echo "<br />";
-}
-echo "数组2键逆序排序： <br />";
-krsort($arr2);
-foreach ($arr2 as $key => $value) {
-    # code...
-    echo $key . $value;
-    echo "<br />";
-}
-echo "数组2值逆序排序：<br />";
-arsort($arr2);
-foreach ($arr2 as $key => $value) {
-    # code...
-    echo $key . $value;
-    echo "<br />";
-}
+    #下面这样的定义方式是允许的，但是不能调用$crr[1],会报错
+    $crr[0] = 'a';
+    $crr[2] = 'c';
+    var_dump($arr);
+    var_dump($brr);
+    var_dump($crr);
+    echo count($crr); //长度为2
+    #遍历数值数组
+    echo '数值数组：<br />';
+    for($i=0;$i<count($arr);$i++)
+    {
+        echo $arr[$i];
+        echo '<br />';
+    }
+    
+    #关联数组
+    echo '关联数组：<br />';
+    $drr = array('name' => 'a', 'code'=> 'b', 'index'=> 'c');
+    $err['name'] = 'a';
+    $err['code'] = 'b';
+    $err['index'] = 'c';
+    foreach ($drr as $key => $value) {
+        # code...
+        echo 'key:'.$key.'. value:'.$value;
+        echo '<br />';
+    }
+    echo '另一种遍历方式：<br />';
+    foreach ($err as $value) {
+        # code...
+        echo $value;
+        echo '<br />';
+    }
+    
 ?>
