27ed05fc7e02dcbce5b76205fbc8fdda08417e06
Fr33Gn3M <freedomd3@gmail.com>
2018-12-13 15:17:11 +0800

    21

---
+++ Python/study/DataTypes&Var.py
@@ -0,0 +1,16 @@
+# 十六进制
+print(0xd34)
+# 浮点数
+print(1.23e5)
+# 转义字符
+print('I\'m \"ok\"')
+# r''忽略转移字符
+print(r'I\'m ok')
+# '''多行
+print('''yes,we
+can do it
+''')
+# r和'''
+print(r'''hello \n
+world
+''')
