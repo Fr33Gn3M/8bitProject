4513f21bae4cb0cea133d0e6808e56b4bcaec4af
Fr33Gn3M <freedomd3@gmail.com>
2019-03-11 18:04:17 +0800

    业务控制器

---
+++ ASP.NET/WebAPI/FD.ModelCreator.WebSerice/FD.ModelCreator.WebSerice/Controllers/BussinessServiceController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TH.ExceptionManager;
+
+namespace FD.ModelCreator.WebSerice.Controllers
+{
+    public class BussinessServiceController : Controller
+    {
+        public JsonResult Query()
+        {
+            var result = new DataBase.JsonResult() { };
+            try
+            {
+                result.Status = MessageTypeCode.Success;
+            }
+            catch (Exception ex)
+            {
+                if (ex is ServiceException)
+                {
+                    result.Status = (ex as ServiceException).Code;
+                    result.Error = (ex as ServiceException).Message;
+                }
+                else
+                {
+                    result.Status = MessageTypeCode.Error;
+                    result.Error = ex.Message;
+                }
+            }
+            return Utility.GetJsonResult(result);
+        }
+
+    }
+}
