d15913517b5941faf5374a0519cb99dbb3ce5257
Fr33Gn3M <freedomd3@gmail.com>
2019-05-22 23:56:49 +0800

    1

---
+++ ASP.NET/MvcService/FD.MvcService/Core/FDJsonResult.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FD.MvcService
+{
+    public class FDJsonResult : JsonResult
+    {
+        public override void ExecuteResult(ControllerContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+
+            HttpResponseBase response = context.HttpContext.Response;
+            if (this.Data != null)
+            {
+                JsonSerializerSettings setting = new JsonSerializerSettings();
+                // 设置日期序列化的格式  
+                setting.DateFormatHandling = DateFormatHandling.IsoDateFormat;
+                response.Write(JsonConvert.SerializeObject(Data, setting));
+            }
+        }
+    }
+}
---
+++ ASP.NET/PublicProject/FD.DataBase/Oracle/OracleDBHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace FD.DataBase
+{
+    public class OracleDBHelper : DataBaseHelper
+    {
+        public OracleDBHelper(string connstr, string modelNameSpace)
+           : base(connstr, modelNameSpace, SqlPrividerType.SqlClient)
+        {
+        }
+
+        public OracleDBHelper(string connstr, string modelNameSpace, Dictionary<string, string> dataBaseKeyFieldDic, Dictionary<string, string> tableNameDic)
+            : base(connstr, modelNameSpace, SqlPrividerType.SqlClient, dataBaseKeyFieldDic, tableNameDic)
+        {
+        }
+    }
+}
