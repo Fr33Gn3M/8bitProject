0e9eba68d0bcaec9f6969cf2399b7eba6402f189
zhi_lian <zhi_lian@ctzh.com>
2023-06-14 18:40:21 +0800

    2023-06-14修改

---
+++ DonNet7/NetApi/FC.BaseApi/FC.BaseApi/Controllers/ShanShanController.cs
@@ -0,0 +1,23 @@
+﻿using FC.Core.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FC.BaseApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShanShanController : ControllerBase
+    {
+        [HttpPost("lawnExcelToWord")]
+        public ApiResult LawnExcelToWord()
+        {
+            var files = Request.Form.Files;
+            if (files.Count == 0) return ApiResult.Error(ApiResultCode.PARAM_ERROR, "请上传Excel");
+
+
+            return ApiResult.Ok();
+        }
+
+
+    }
+}
---
+++ JAVA/OriginService/src/test/java/com/freedom/origin/OriginServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freedom.origin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OriginServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
